cba04b1ec7b47e98aa4f03e660b0df4339a51f64
Robraym <robson.raymundo@gmail.com>
2026-05-03 13:50:59 -0300

    Ajuste no layout Ajuste na logica do disparo do alarme

--- app/src/main/java/ray/droid/com/droidalarmclock/MainActivity.java
+++ app/src/main/java/ray/droid/com/droidalarmclock/MainActivity.java
@@ -16,10 +16,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowInsets;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.TimePicker;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.lang.reflect.Array;
@@ -30,7 +34,16 @@ import java.util.concurrent.TimeUnit;
 public class MainActivity extends AppCompatActivity {
 
     private Context context;
-    private TimePicker timePicker;
+    private int selectedHour;
+    private int selectedMinute;
+    private TextView hourPrevious;
+    private TextView hourValue;
+    private TextView hourNext;
+    private TextView minutePrevious;
+    private TextView minuteValue;
+    private TextView minuteNext;
+    private LinearLayout hourColumn;
+    private LinearLayout minuteColumn;
     private long segundos = 1000;
     private long minutos = segundos * 5;
     private long horas = minutos * 60;
@@ -43,15 +56,28 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox chkSab;
     private CheckBox chkDom;
     private Button btnAgendar;
+    private int mainScrollDefaultBottomPadding;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ConfigureSystemInsets();
 
-        timePicker = (TimePicker) findViewById(R.id.timePicker);
-        timePicker.setIs24HourView(true);
+        hourPrevious = (TextView) findViewById(R.id.hourPrevious);
+        hourValue = (TextView) findViewById(R.id.hourValue);
+        hourNext = (TextView) findViewById(R.id.hourNext);
+        minutePrevious = (TextView) findViewById(R.id.minutePrevious);
+        minuteValue = (TextView) findViewById(R.id.minuteValue);
+        minuteNext = (TextView) findViewById(R.id.minuteNext);
+        hourColumn = (LinearLayout) findViewById(R.id.hourColumn);
+        minuteColumn = (LinearLayout) findViewById(R.id.minuteColumn);
+        ConfigureTimeControls();
+        Calendar now = Calendar.getInstance();
+        selectedHour = now.get(Calendar.HOUR_OF_DAY);
+        selectedMinute = now.get(Calendar.MINUTE);
+        UpdateTimeControls();
         
         context = getBaseContext();
 
@@ -74,6 +100,114 @@ public class MainActivity extends AppCompatActivity {
         });
 
         LoadPreferences();
+    }
+
+    private void ConfigureSystemInsets() {
+        final ScrollView mainScroll = (ScrollView) findViewById(R.id.mainScroll);
+        mainScrollDefaultBottomPadding = mainScroll.getPaddingBottom();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
+            mainScroll.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
+                @Override
+                public WindowInsets onApplyWindowInsets(View view, WindowInsets insets) {
+                    view.setPadding(
+                            view.getPaddingLeft(),
+                            view.getPaddingTop(),
+                            view.getPaddingRight(),
+                            mainScrollDefaultBottomPadding + insets.getSystemWindowInsetBottom()
+                    );
+                    return insets;
+                }
+            });
+        }
+    }
+
+    private void ConfigureTimeControls() {
+        hourColumn.setOnTouchListener(CreateTimeColumnTouchListener(true));
+        minuteColumn.setOnTouchListener(CreateTimeColumnTouchListener(false));
+    }
+
+    private View.OnTouchListener CreateTimeColumnTouchListener(final boolean isHour) {
+        return new View.OnTouchListener() {
+            private float startY;
+
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    startY = event.getY();
+                    return true;
+                }
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    float deltaY = event.getY() - startY;
+
+                    if (Math.abs(deltaY) > 20) {
+                        ChangeTimeWithAnimation(view, isHour, deltaY < 0);
+                    } else {
+                        ChangeTimeWithAnimation(view, isHour, event.getY() >= view.getHeight() / 2f);
+                    }
+
+                    view.performClick();
+                    return true;
+                }
+
+                return true;
+            }
+        };
+    }
+
+    private void ChangeTimeWithAnimation(final View column, final boolean isHour, final boolean increment) {
+        final int direction = increment ? 1 : -1;
+
+        column.animate()
+                .translationY(direction * -18)
+                .alpha(0.72f)
+                .setDuration(80)
+                .withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (isHour) {
+                            selectedHour = WrapValue(selectedHour + direction, 24);
+                        } else {
+                            selectedMinute = WrapValue(selectedMinute + direction, 60);
+                        }
+
+                        UpdateTimeControls();
+                        column.setTranslationY(direction * 18);
+                        column.animate()
+                                .translationY(0)
+                                .alpha(1f)
+                                .setDuration(90)
+                                .start();
+                    }
+                })
+                .start();
+    }
+
+    private int WrapValue(int value, int max) {
+        if (value < 0) {
+            return max - 1;
+        }
+
+        if (value >= max) {
+            return 0;
+        }
+
+        return value;
+    }
+
+    private void UpdateTimeControls() {
+        hourPrevious.setText(FormatTwoDigits(WrapValue(selectedHour - 1, 24)));
+        hourValue.setText(FormatTwoDigits(selectedHour));
+        hourNext.setText(FormatTwoDigits(WrapValue(selectedHour + 1, 24)));
+
+        minutePrevious.setText(FormatTwoDigits(WrapValue(selectedMinute - 1, 60)));
+        minuteValue.setText(FormatTwoDigits(selectedMinute));
+        minuteNext.setText(FormatTwoDigits(WrapValue(selectedMinute + 1, 60)));
+    }
+
+    private String FormatTwoDigits(int value) {
+        return String.format("%02d", value);
     }
 
     private void LoadPreferences()
@@ -108,8 +242,9 @@ public class MainActivity extends AppCompatActivity {
             int prefHour = DroidPreferences.GetInteger(context, "timePickerHour");
             int prefMinute = DroidPreferences.GetInteger(context, "timePickerMinute");
 
-            timePicker.setCurrentHour(prefHour);
-            timePicker.setCurrentMinute(prefMinute);
+            selectedHour = prefHour;
+            selectedMinute = prefMinute;
+            UpdateTimeControls();
 
 
         }
@@ -150,10 +285,10 @@ public class MainActivity extends AppCompatActivity {
         try {
             ArrayList<Integer> daysOfWeek = SetDaysOfWeek();
             DroidPreferences.SetString(context, "DaysOfWeek", daysOfWeek.toString());
-            DroidPreferences.SetInteger(context, "timePickerHour", timePicker.getCurrentHour());
-            DroidPreferences.SetInteger(context, "timePickerMinute", timePicker.getCurrentMinute());
+            DroidPreferences.SetInteger(context, "timePickerHour", selectedHour);
+            DroidPreferences.SetInteger(context, "timePickerMinute", selectedMinute);
 
-            Calendar calendar = Others.ScheduleAlarm(context, daysOfWeek, timePicker.getCurrentHour(), timePicker.getCurrentMinute());
+            Calendar calendar = Others.ScheduleAlarm(context, daysOfWeek, selectedHour, selectedMinute);
 
             Toast.makeText(context, "Alarme agendado com sucesso! ", Toast.LENGTH_LONG).show();
 
